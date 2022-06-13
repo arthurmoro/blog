7fe76b4a8ab1eb1693ffc4300a38f88b2a2fb9c9
Arthur Moro <arthurmoro@gmail.com>
2022-06-13 20:03:19 -0300

    build for next

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "/styles/globals.css"
 import "/styles/post-page.css"
-import Header from "/components/Header"
+import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
   return (
