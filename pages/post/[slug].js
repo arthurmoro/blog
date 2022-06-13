80e3904222ba7fb45814e74e982a045eeb90e6a2
Arthur Moro <arthurmoro@gmail.com>
2022-06-13 20:56:21 -0300

    compile

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import fs from "fs"
 import path from "path";
 import matter from "gray-matter";
-import Post from "components/Post"
+import Post from "components/Post/index.js"
 
 
 export default function Blog({ post }) {
