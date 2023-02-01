ba49ae04f8a95dcb5ae455afa87ae856fbdc7b76
Arthur Moro <arthurmoro@gmail.com>
2023-02-01 09:08:32 -0300

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,16 @@ import Footer from "@/components/Footer"
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LNDBCZFX4Q" />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LNDBCZFX4Q');
+          `
+        }} />
       <Header />
       <main className="max-w-7xl my-0 mx-auto">
         <Component {...pageProps} />
