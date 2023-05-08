325c7f213e52bc1e553b8cdb256593869e3fd66f
Arthur Moro <arthurmoro@gmail.com>
2023-05-08 18:09:25 -0300

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "styles/bootstrap.min.css"
 import "styles/post-page.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import Script from "next/script"
 import TagManager from "react-gtm-module"
 import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-LNDBCZFX4Q' })
+
+    TagManager.initialize({ gtmId: 'G-LNDBCZFX4Q' })
   })
   return (
     <>
