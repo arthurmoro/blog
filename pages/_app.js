1e1b83a1f747d77bdd3f425faf4de3dc5303ae73
Arthur Moro <arthurmoro@gmail.com>
2023-05-08 18:07:55 -0300

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,15 @@ import "styles/bootstrap.min.css"
 import "styles/post-page.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import Script from "next/script"
+import TagManager from "react-gtm-module"
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-LNDBCZFX4Q' })
+  })
   return (
     <>
-      <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-LNDBCZFX4Q" />
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){ 
-              dataLayer.push(arguments); 
-            }
-            gtag('js', new Date());
-            gtag('config', 'G-LNDBCZFX4Q');
-          `
-        }} />
       <Header />
       <Component {...pageProps} />
       <Footer />
